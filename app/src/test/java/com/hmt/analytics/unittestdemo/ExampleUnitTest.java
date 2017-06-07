456306f99f0035e7b0fc5113fd01554eee982931
renbo <renbo.ma@hypers.com>
2017-06-07 15:17:34 +0800

    add mockito and robolectric dependences

--- app/src/test/java/com/hmt/analytics/unittestdemo/ExampleUnitTest.java
+++ app/src/test/java/com/hmt/analytics/unittestdemo/ExampleUnitTest.java
@@ -1,8 +1,15 @@
 package com.hmt.analytics.unittestdemo;
 
-import org.junit.Test;
+import com.hmt.analytics.common.CommonUtil;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +20,29 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testMock() {
+        List mockedList = mock(List.class);
+        mockedList.add("one");
+        mockedList.clear();
+        Mockito.verify(mockedList).add("one");
+//        Mockito.verify(mockedList).add("two");
+        Mockito.verify(mockedList).clear();
+    }
+
+    @Test
+    public void testMock2() {
+        ArrayList mockedList = mock(ArrayList.class);
+        Mockito.when(mockedList.get(0)).thenReturn("1");
+        String str = (String) mockedList.get(0);
+        Mockito.verify(mockedList).get(0);
+    }
+
+    @Test
+    public void commonUtilsTest() {
+        CommonUtil commonUtil = Mockito.mock(CommonUtil.class);
+
     }
 }
