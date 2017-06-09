d99c9d09acce23d270b25d9ab1f10e6f5efd78f3
renbo <renbo.ma@hypers.com>
2017-06-09 15:57:56 +0800

    add mockito rules

--- app/src/test/java/com/hmt/analytics/unittestdemo/ExampleUnitTest.java
+++ app/src/test/java/com/hmt/analytics/unittestdemo/ExampleUnitTest.java
@@ -6,10 +6,13 @@ import org.mockito.Mockito;
 
 import java.util.List;
 
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -41,6 +44,13 @@ public class ExampleUnitTest {
         mCalculator.divide(1, 0);
     }
 
+    @Test
+    public void testVerify() {
+        VerifyPsw psw = new VerifyPsw();
+        assertTrue(psw.verifyValid("1", "1"));
+        assertFalse(psw.verifyValid("11", "1"));
+    }
+
 
     @Test
     public void testMock() {
@@ -58,19 +68,43 @@ public class ExampleUnitTest {
 
     /**
      * mockito 测试是否调用
+     * mock 不会走实际的逻辑 只会返回默认值
      */
     @Test
     public void testWhen() {
 //        LoginPresent loginPresent = mock(LoginPresent.class);
         LoginPresent loginPresent = new LoginPresent();
         VerifyPsw verifyPsw = mock(VerifyPsw.class);
+        log("true === " + verifyPsw.verifyValid("1", "1"));
+        log("false === " + verifyPsw.verifyValid("1", "11"));
         loginPresent.setVerifyPsw(verifyPsw);
         loginPresent.login("1", "0");
+
         when(verifyPsw.verifyValid(Mockito.anyString(), Mockito.anyString()))
                 .thenReturn(true);
+
 //        verify(loginPresent).login("1", "0");
-        verify(verifyPsw).verifyValid("1", "0");
+
+//        boolean result = doCallRealMethod().when(verifyPsw).verifyValid("1", "1");
+//       when(verifyPsw.verifyValid("0", "1")).thenCallRealMethod();
+
     }
 
+    /**
+     * spy 会走实际的逻辑
+     */
+    @Test
+    public void testSpy() {
+        VerifyPsw verifyPsw = spy(VerifyPsw.class);
+        log("true === " + verifyPsw.verifyValid("1", "1"));
+        log("false === " + verifyPsw.verifyValid("1", "11"));
+
+
+//        doReturn(true).when(verifyPsw).verifyValid("2", "1");
+    }
+
+    public void log(String text) {
+        System.out.println("text = " + text);
+    }
 
 }
